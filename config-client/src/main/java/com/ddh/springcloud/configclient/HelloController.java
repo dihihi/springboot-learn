0448375873beb3257fa87176920ae471ff672cd3
dihihi <q.yahoo@qq.com>
2019-03-21 17:51:57 +0800

    config client

--- config-client/src/main/java/com/ddh/springcloud/configclient/HelloController.java
+++ config-client/src/main/java/com/ddh/springcloud/configclient/HelloController.java
@@ -1,24 +1,19 @@
-package com.ddh.springboot.first;
+package com.ddh.springcloud.configclient;
 
-import com.ddh.springboot.first.dao.UserDao;
-import com.ddh.springboot.first.domain.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * Created by ddh on 2019/3/14.
  */
 @RestController
 public class HelloController {
-	@Autowired private UserDao userDao;
+	@Value("${foo}")
+	private String foo;
 
-	@RequestMapping(value = "/hello")
-	public String hello() {
-		List<User> users = userDao.findAll();
-		System.out.println(users.size());
-		return "hello world2.";
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
 	}
 }
